babab129fc4840ec133a48e7e5ddd109adccf348
Kharif <ledamecrydenfalk@gmail.com>
2026-07-24 22:33:39 +0200

    chore: bump formula to v0.1.32

--- Formula/ce.rb
+++ Formula/ce.rb
@@ -6,7 +6,7 @@
 class Ce < Formula
   desc "Peer-to-peer compute mesh and economy"
   homepage "https://github.com/ce-net/ce"
-  version "0.1.31"
+  version "0.1.32"
   license "MIT"
 
   on_macos do
