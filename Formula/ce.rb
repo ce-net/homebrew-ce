10d154fadc50aa8c99cfa9b1a94481c196db9f5d
Leif Rydenfalk <ledamecrydenfalk@gmail.com>
2026-07-27 19:07:53 +0200

    chore: real v0.1.33 checksums (the updater had been leaving the first release's sums in place)

--- Formula/ce.rb
+++ Formula/ce.rb
@@ -12,7 +12,7 @@ class Ce < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ce-net/ce/releases/download/v#{version}/ce-macos-arm64.tar.gz"
-      sha256 "fc518794fbcbc9b202ea8644589705a604cbcf3c85d1954e880bc2670b0508da"
+      sha256 "20879439975556cf281568d2d6084c3b518655a353f5586b9320b7b0bbcab423"
     else
       odie "ce ships no Intel-Mac binary. Build from source (cargo build --release) or run: curl -sSL https://ce-net.com/install.sh | bash"
     end
@@ -21,10 +21,10 @@ class Ce < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ce-net/ce/releases/download/v#{version}/ce-linux-arm64.tar.gz"
-      sha256 "d02e7f089e909bb4694b843dc3e293501e6116230e67e15902ee560afaad0b42"
+      sha256 "04e8bd17348350cc5b5bfbded96f1542043f231f792a55d0d89ae525b99d2ef3"
     else
       url "https://github.com/ce-net/ce/releases/download/v#{version}/ce-linux-amd64.tar.gz"
-      sha256 "7611ebefde6ec1f2935f90ddd2661cbb0f58b63247a8ff268a1572790f302b46"
+      sha256 "00bd39853d3e11b97387e60e6085d250e2c16dce1aa37d60a011bc2b5268f0db"
     end
   end
 
