015bb205c1af07e3e700bc1f02fdd272babd1224
Leif Rydenfalk <ledamecrydenfalk@gmail.com>
2026-07-27 19:04:22 +0200

    chore: bump formula to v0.1.33

--- Formula/ce.rb
+++ Formula/ce.rb
@@ -6,7 +6,7 @@
 class Ce < Formula
   desc "Peer-to-peer compute mesh and economy"
   homepage "https://github.com/ce-net/ce"
-  version "0.1.32"
+  version "0.1.33"
   license "MIT"
 
   on_macos do
